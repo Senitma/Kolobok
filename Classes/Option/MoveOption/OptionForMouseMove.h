70bb9b62d5ce0c7a468bcc6a86acf1694721f48f
Sergei <senitma@gmail.com>
2018-03-05 20:54:51 +0600

    Добавление опции для перемещения с помощью мыши

---
+++ Classes/Option/MoveOption/OptionForMouseMove.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "OptionForMove.h"
+#include "..\Option\BaseClass\ISMouse.h"
+
+// Опция для перемещения
+class OptionForMouseMove : public OptionForMove, public ISMouse
+{
+public:
+	void MouseClick(MouseType type, int x, int y) override;
+};
